d2578b3e87f6b0d71a07fa339c953b5d57288229
Magno Pereira <elmagnopc@gmail.com>
2017-05-16 23:37:05 -0300

    exercicios

---
+++ google-python-exercises/basic/string2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python2.4 -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Additional basic string exercises
+
+
+# D. verbing
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+def verbing(s):
+    retorno = s
+    if len(s) >2:
+        if s.endswith('ing'):
+            retorno += 'ly'
+        else:
+            retorno += 'ing'
+
+    # +++your code here+++
+    return retorno
+
+
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+    pos_not = s.rfind('not')
+    pos_bad = s.rfind('bad')
+    if (pos_bad >= 0 and pos_not >=0) and (pos_not< pos_bad):
+        s = s.replace(s[pos_not:pos_bad+3], 'good')
+    #print(s)
+    # +++your code here+++
+    return s
+
+
+# F. front_back
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+def front_back(a, b):
+    # +++your code here+++
+    tam_a = len(a)
+    tam_b = len(b)
+
+    if tam_a % 2 ==0:
+        meio_a = int(tam_a/2)
+    else:
+        meio_a = int(tam_a/2)+1
+
+    if tam_b % 2 ==0:
+        meio_b = int(tam_b/2)
+    else:
+        meio_b = int(tam_b/2)+1
+    retorno = a[0:meio_a] + b[0:meio_b] + a[meio_a:]+a[meio_b:]
+    return retorno
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# main() calls the above functions with interesting inputs,
+# using the above test() to check if the result is correct or not.
+def main():
+    print('verbing')
+    test(verbing('hail'), 'hailing')
+    test(verbing('swiming'), 'swimingly')
+    test(verbing('do'), 'do')
+
+    print()
+    print('not_bad')
+    test(not_bad('This movie is not so bad'), 'This movie is good')
+    test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+    test(not_bad('This tea is not hot'), 'This tea is not hot')
+    test(not_bad("It's bad yet not"), "It's bad yet not")
+
+    print()
+    print('front_back')
+    test(front_back('abcd', 'xy'), 'abxcdy')
+    test(front_back('abcde', 'xyz'), 'abcxydez')
+    test(front_back('Kitten', 'Donut'), 'KitDontenut')
+
+
+if __name__ == '__main__':
+    main()
